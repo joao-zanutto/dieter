0a5eb0d0b0af9490bca9eb236eb55951186f05e8
zanuttin <jpberno@gmail.com>
2021-03-11 22:25:48 -0300

    Extracted create food form to a Dilog with a button on the main App to open it

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
+import { Button } from '@material-ui/core';
 import { useState } from 'react';
 import CreateFoodForm from './components/CreateFoodForm';
 
 function App() {
 	const [foodList, setFoodList] = useState([]);
+	const [open, setOpen] = useState(false);
 
 	const addFoodToList = (food) => {
 		setFoodList([...foodList, food]);
 	};
 
+	const openDialog = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
 	return (
 		<div className='App'>
-			<CreateFoodForm addFoodToList={addFoodToList} />
+			<CreateFoodForm
+				addFoodToList={addFoodToList}
+				open={open}
+				handleClose={handleClose}
+			/>
+			<Button onClick={openDialog} variant='contained' color='primary'>
+				{' '}
+				Adicionar Comida{' '}
+			</Button>
+
 			<p>{JSON.stringify(foodList, null, 2)}</p>
 		</div>
 	);
--- src/components/CreateFoodForm.js
+++ src/components/CreateFoodForm.js
@@ -2,6 +2,8 @@ import {
 	Button,
 	Card,
 	CardContent,
+	Dialog,
+	DialogContent,
 	Divider,
 	Grid,
 	TextField,
@@ -10,7 +12,7 @@ import {
 import { useFormik } from 'formik';
 import MiniFoodCard from './MiniFoodCard';
 
-function CreateFoodForm({ addFoodToList }) {
+function CreateFoodForm({ addFoodToList, open, handleClose }) {
 	const formik = useFormik({
 		initialValues: {
 			foodName: '',
@@ -40,112 +42,130 @@ function CreateFoodForm({ addFoodToList }) {
 		},
 	});
 
+	const resetFields = () => {
+		formik.setValues(formik.initialValues);
+	};
+
 	return (
-		<Card elevation={3}>
-			<CardContent>
-				<Grid container justify='space-between' spacing={4}>
-					<Grid item xs={12}>
-						<Typography variant='h5'> Criação de Comida </Typography>
-					</Grid>
+		<Dialog open={open} onClose={handleClose}>
+			<DialogContent>
+				<Card elevation={0}>
+					<CardContent>
+						<Grid container justify='space-between' spacing={4}>
+							<Grid item xs={12}>
+								<Typography variant='h5'> Criação de Comida </Typography>
+							</Grid>
 
-					<Grid item xs={12}>
-						<Divider />
-					</Grid>
+							<Grid item xs={12}>
+								<Divider />
+							</Grid>
 
-					<Grid item container xs={12} spacing={2}>
-						<Grid item xs={12}>
-							<TextField
-								fullWidth
-								autocomplete='off'
-								variant='outlined'
-								label='Nome do alimento'
-								value={formik.values.foodName}
-								onChange={formik.handleChange}
-								name='foodName'
-							/>
+							<Grid item container xs={12} spacing={2}>
+								<Grid item xs={12}>
+									<TextField
+										fullWidth
+										autocomplete='off'
+										variant='outlined'
+										label='Nome do alimento'
+										value={formik.values.foodName}
+										onChange={formik.handleChange}
+										name='foodName'
+									/>
+								</Grid>
+
+								<Grid item xs={12}>
+									<TextField
+										fullWidth
+										autocomplete='off'
+										variant='outlined'
+										label='Porção do alimento'
+										value={formik.values.portion}
+										onChange={formik.handleChange}
+										name='portion'
+									/>
+								</Grid>
+							</Grid>
+
+							<Grid item xs={12}>
+								<Divider />
+							</Grid>
+
+							<Grid item container xs={12} sm={6} spacing={2}>
+								<Grid item xs={12}>
+									<TextField
+										fullWidth
+										autocomplete='off'
+										variant='outlined'
+										label={`Carboídratos (g/${formik.values.portion})`}
+										value={formik.values.carb}
+										onChange={formik.handleChange}
+										name='carb'
+										type='number'
+									/>
+								</Grid>
+
+								<Grid item xs={12}>
+									<TextField
+										fullWidth
+										autocomplete='off'
+										variant='outlined'
+										label={`Proteínas (g/${formik.values.portion})`}
+										value={formik.values.prot}
+										onChange={formik.handleChange}
+										name='prot'
+										type='number'
+									/>
+								</Grid>
+
+								<Grid item xs={12}>
+									<TextField
+										fullWidth
+										autocomplete='off'
+										variant='outlined'
+										label={`Gorduras (g/${formik.values.portion})`}
+										value={formik.values.fat}
+										onChange={formik.handleChange}
+										name='fat'
+										type='number'
+									/>
+								</Grid>
+							</Grid>
+
+							<Grid item xs={11} sm={6}>
+								<MiniFoodCard
+									foodName={formik.values.foodName}
+									portion={formik.values.portion}
+									carb={formik.values.carb}
+									prot={formik.values.prot}
+									fat={formik.values.fat}
+								/>
+							</Grid>
+
+							<Grid item container alignContent='right' spacing={2}>
+								<Grid item>
+									<Button
+										onClick={formik.handleSubmit}
+										raised
+										variant='contained'
+										color='primary'>
+										Salvar
+									</Button>
+								</Grid>
+								<Grid item>
+									<Button
+										onClick={resetFields}
+										raised
+										variant='contained'
+										color='secondary'>
+										Limpar
+									</Button>
+								</Grid>
+							</Grid>
 						</Grid>
-
-						<Grid item xs={12}>
-							<TextField
-								fullWidth
-								autocomplete='off'
-								variant='outlined'
-								label='Porção do alimento'
-								value={formik.values.portion}
-								onChange={formik.handleChange}
-								name='portion'
-							/>
-						</Grid>
-					</Grid>
-
-					<Grid item xs={12}>
-						<Divider />
-					</Grid>
-
-					<Grid item container xs={12} sm={6} spacing={2}>
-						<Grid item xs={12}>
-							<TextField
-								fullWidth
-								autocomplete='off'
-								variant='outlined'
-								label={`Carboídratos (g/${formik.values.portion})`}
-								value={formik.values.carb}
-								onChange={formik.handleChange}
-								name='carb'
-								type='number'
-							/>
-						</Grid>
-
-						<Grid item xs={12}>
-							<TextField
-								fullWidth
-								autocomplete='off'
-								variant='outlined'
-								label={`Proteínas (g/${formik.values.portion})`}
-								value={formik.values.prot}
-								onChange={formik.handleChange}
-								name='prot'
-								type='number'
-							/>
-						</Grid>
-
-						<Grid item xs={12}>
-							<TextField
-								fullWidth
-								autocomplete='off'
-								variant='outlined'
-								label={`Gorduras (g/${formik.values.portion})`}
-								value={formik.values.fat}
-								onChange={formik.handleChange}
-								name='fat'
-								type='number'
-							/>
-						</Grid>
-					</Grid>
-
-					<Grid item xs={12} sm={6}>
-						<MiniFoodCard
-							foodName={formik.values.foodName}
-							portion={formik.values.portion}
-							carb={formik.values.carb}
-							prot={formik.values.prot}
-							fat={formik.values.fat}
-						/>
-					</Grid>
-
-					<Grid item alignContent='right'>
-						<Button
-							onClick={formik.handleSubmit}
-							raised
-							align='right'
-							variant='contained'
-							color='primary'>
-							Salvar
-						</Button>
-					</Grid>
-				</Grid>
-			</CardContent>
-		</Card>
+					</CardContent>
+				</Card>
+			</DialogContent>
+		</Dialog>
 	);
 }
 
